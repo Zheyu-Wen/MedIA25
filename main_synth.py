6ef1986f1686c9aa3a1836d2c19a2597db3ef91d
Zheyu Wen <zheyw@umich.edu>
2026-03-18 11:21:06 -0500

    synth

--- main_synth.py
+++ main_synth.py
@@ -6,14 +6,16 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Multi-biomarkers')
     parser.add_argument('--noise_level', default=0) ## example 0.1
+    parser.add_argument('--project_path', default='.')
+    parser.add_argument('--save_folder', default='./results')
     args = parser.parse_args()
 
     ## test synthetic sensitivity to initial guess
 
-    project_path = '/home1/08171/zheyw1/Alzh_tau_abeta_model_new'
+    project_path = str(args.project_path)
     alz = pde_class.Alzh_model_class(project_path)
     alz.noise_level = float(args.noise_level)
-    alz.save_folder = '/scratch/08171/zheyw1/MedIA25_results/MedIA25_synth_inversion'
+    alz.save_folder = str(args.save_folder)
 
     os.makedirs(alz.save_folder, exist_ok=True)
     alz.synthetic_inversion()
